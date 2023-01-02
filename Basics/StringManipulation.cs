ea21d8d51b5dbcbb11b2dec17ba9f0c5705fdf3e
Pratyush Sharma <37974007+Pratyushsharmaa@users.noreply.github.com>
2023-01-02 15:33:11 +0530

    Add files via upload

---
+++ Basics/StringManipulation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Stringmanipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Ram";
+            int age = 28;
+            string job = "Engineer";
+            {
+                //string concatenation
+                Console.WriteLine("String Concatenation");
+                Console.WriteLine("Hi!" + "My name is " + name + " and age is " + age + " and job is " + job);
+                
+
+                //string formatting
+                Console.WriteLine("String Formatting");
+                Console.WriteLine("Hi!My name is {0} and age is {1} and job is {2}", name, age, job);
+                
+
+                //string interpolation
+                //use $ at the start
+                Console.WriteLine("String interpolation");
+                Console.WriteLine($"Hi!My name is {name} and age is {age} and job is {job}");
+            }
+
+        }
+    }
+}
