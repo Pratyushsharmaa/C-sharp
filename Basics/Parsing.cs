13dfdb494217254a55070be4810df388e1a1052f
Pratyush Sharma <37974007+Pratyushsharmaa@users.noreply.github.com>
2023-01-02 14:40:46 +0530

    Add files via upload

---
+++ Basics/Parsing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Parsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstString = "13";
+            string secondString = "17";
+            int num1 = Int32.Parse(firstString);
+            int num2 = Int32.Parse(secondString);
+            int result = num1 + num2;
+            Console.WriteLine(result);
+            Console.Read();
+        }
+    }
+}
