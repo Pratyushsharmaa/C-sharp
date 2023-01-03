ff60b1efbf37fa0ef0834d93ddbdb5cf7f1560cb
Pratyush Sharma <37974007+Pratyushsharmaa@users.noreply.github.com>
2023-01-03 13:54:12 +0530

    Add files via upload

---
+++ Decisions/TemperatureCheck.cs
@@ -0,0 +1,30 @@
+﻿//if-else using try parse method
+
+using System;
+
+namespace TemperatureCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter current temperature");
+            string temperature = Console.ReadLine();
+            int numTemp = 0; 
+            int number;
+            if(int.TryParse(temperature,out number))
+            {
+                numTemp = number;
+            }
+            if (numTemp < 20)
+            {
+                Console.WriteLine("Winters");
+            }
+            else
+            {
+                Console.WriteLine("Summers");
+            }
+        }
+    }
+}
