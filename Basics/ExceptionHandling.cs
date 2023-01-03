5908060daa7517631519cb4aca28a6e34f2d4e48
Pratyush Sharma <37974007+Pratyushsharmaa@users.noreply.github.com>
2023-01-03 13:14:40 +0530

    Add files via upload

---
+++ Basics/ExceptionHandling.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            string userInput = Console.ReadLine();
+            try
+            {
+                int userInputAsInt = int.Parse(userInput);
+            }
+            catch(FormatException) //for wrong format of input
+            {
+
+                Console.WriteLine("Wrong input!");
+            }
+            catch(OverflowException)  //if string input is too large
+            {
+                Console.WriteLine("Overflow");
+            }
+            finally                  //this is always written at last with try-catch, even if no errors
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
