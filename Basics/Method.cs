1000319671e46a9d57e5a9e4dfeba8b7ce21b9d7
Pratyush Sharma <37974007+Pratyushsharmaa@users.noreply.github.com>
2023-01-03 11:22:07 +0530

    Add files via upload

---
+++ Basics/Method.cs
@@ -0,0 +1,26 @@
+﻿//Basic code to create a method and call it.
+
+using System;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random();                                    //method is called
+            RandomSpecific("Called from method named RandomSpecific");
+        }
+            public static void Random()                  //method is created
+            {
+                Console.WriteLine("Called from method named Random");
+                
+            }
+            public static void RandomSpecific(string myText)
+        {
+            Console.WriteLine(myText);
+
+        }
+        }
+    }
+
