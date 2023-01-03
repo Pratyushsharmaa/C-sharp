d3937c3e26aab7fedcde30a30743a9a27b229e15
Pratyush Sharma <37974007+Pratyushsharmaa@users.noreply.github.com>
2023-01-03 11:34:15 +0530

    Add files via upload

---
+++ Basics/AdditionUsingMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AdditionUsingMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Add(10,20));
+        }
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
