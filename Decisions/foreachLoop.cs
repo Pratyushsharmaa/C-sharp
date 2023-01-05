6bfe8d1dec575fd17ca014b33e31e810d90ca280
Pratyush Sharma <37974007+Pratyushsharmaa@users.noreply.github.com>
2023-01-05 13:14:09 +0530

    Add files via upload

---
+++ Decisions/foreachLoop.cs
@@ -0,0 +1,20 @@
+﻿//Greet friends using foreach loop.
+
+using System;
+
+namespace foreachloop
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        string[] myFriends = { "David", "Chris", "Male", "Shiv" };
+        foreach (string name in myFriends)
+        {
+            Console.WriteLine("Hi,my friend {0}", name);
+        }
+        Console.ReadKey();
+    }
+}
+}
+
