894980dd27c5669f826ad3dc066c1223afa47ec3
Pratyush Sharma <37974007+Pratyushsharmaa@users.noreply.github.com>
2023-01-09 02:17:49 +0530

    Add files via upload

---
+++ Collections/list.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic; //List is a generic collection
+namespace list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> li = new List<int>();
+            li.Add(10);
+            li.Add(11);
+            li.Add(12);
+            li.Add(13);
+            
+            foreach (int i in li) //print all values
+            {
+                Console.WriteLine(i+ " ");
+                Console.ReadLine();
+            }
+
+            // Insert values in list
+            li.Insert(2, 15);
+            foreach (int i in li) //print all values
+            {
+                Console.WriteLine(i + " ");
+                Console.ReadLine();
+            }
+
+            // Delete values from list
+            li.RemoveAt(3);
+            foreach (int i in li) //print all values
+            {
+                Console.WriteLine(i + " ");
+                Console.ReadLine();
+            }
+        }
+    }
+}
