2b4a485b1600c32b6299b556c1481f485be80711
Pratyush Sharma <37974007+Pratyushsharmaa@users.noreply.github.com>
2023-01-08 23:11:02 +0530

    Add files via upload

---
+++ OOP/ClassExercise1/Human.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassExercise1
+{
+    class Human
+    {
+        public string firstName;
+        public string lastName;
+
+        public void IntroduceMyself()     //member variable
+        {
+            Console.WriteLine("Hi! My name is: {0} {1}", firstName, lastName);
+        }
+    }
+}
